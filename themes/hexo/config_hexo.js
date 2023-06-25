6f3e5b7b810ba08ecb00f86a235a39f8dfc0c62c
Andy Chu <101468637+Zhu-Shatong@users.noreply.github.com>
2023-06-25 15:27:24 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个数据科学本科生', 'Hi，我目前研究强化学习', 'Hi，我目前研究计量经济学', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Hi，我来自同济大学计算机系', 'Hi，我目前研究多智能体强化学习MARL', 'Hi，我目前研究计算经济学CE', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
